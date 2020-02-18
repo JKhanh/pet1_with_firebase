6ca81e01613c6b394702a1ffe80eac41ce8b4ccb
Noone <nguyenkhanhcv@gmail.com>
2020-02-18 22:29:29 +0700

    Add firebase authentication

---
+++ lib/root_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:com_jonas_pet/login_signup_page.dart';
+import 'package:com_jonas_pet/authentication.dart';
+import 'package:com_jonas_pet/homepage.dart';
+
+enum AuthStatus{
+  NOT_DETERMINED,
+  MOT_LOGGED_IN,
+  LOGGED_IN,
+}
+
